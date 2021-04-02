486dfcaea7aa6ecced4ecc2db8bf4dac80d33d9c
Etienne Robin <etienner0601@gmail.com>
2021-04-02 13:13:19 -0700

    Naive Palindrome impl. and cpp properties added

--- PalindromeLinkedList.cpp
+++ PalindromeLinkedList.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
 // Definition for singly-linked list.
 struct ListNode {
@@ -9,8 +12,48 @@ struct ListNode {
 };
 
 class Solution {
+private:
+    bool reverseCheck(vector<int>& toCheck, ListNode* checkPtr) {
+        int i;
+        ListNode* currPtr = checkPtr;
+        // first check if it's an even-numbered palindrome
+        for (i = toCheck.size() - 1; i >= 0 && currPtr != nullptr; --i) {
+            if (toCheck[i] != currPtr->val) {
+                break;
+            }
+            currPtr = currPtr->next;
+        }
+        if (i == -1 && currPtr == nullptr) {
+            // then it's a valid even-numbered palindrome
+            return true;
+        }
+
+        // now check if it's an odd-numbered palindrome
+        currPtr = checkPtr;
+        for (i = toCheck.size() - 2; i >= 0 && currPtr != nullptr; --i) {
+            if (toCheck[i] != currPtr->val) {
+                break;
+            }
+            currPtr = currPtr->next;
+        }
+
+        return (i == -1 && currPtr == nullptr);
+    }
 public:
     bool isPalindrome(ListNode* head) {
-        
+        // check the trivial case first
+        if (head->next == nullptr) {
+            return true;
+        }
+        vector<int> vals;
+        ListNode* currPtr = head;
+        while (currPtr != nullptr) {
+            if (reverseCheck(vals, currPtr)) {
+                return true;
+            }
+            vals.push_back(currPtr->val);
+            currPtr = currPtr->next;
+        }
+        return false;
     }
 };
