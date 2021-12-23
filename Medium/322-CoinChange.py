0fd2fc4b7d6f81391cb077aafb876c995c46d8a5
Etienne Robin <etienner0601@gmail.com>
2021-12-23 14:35:24 -0800

    solved 322 coin change using DP

--- Medium/322-CoinChange.py
+++ Medium/322-CoinChange.py
@@ -1,21 +1,27 @@
 class Solution:
-    
-    def minNumCoins(coins: List[int], amount: int) -> int:
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        # if amount is zero then you don't need any coins
         if amount == 0:
             return 0
         
-        numCoins = []
+        # initialize dynamic programming array
+        dp = [10001] * (amount + 1)
         
-        for coinAmt in coins:
-            if coinAmt <= amount:
-                subProbMinCoins = Solution.minNumCoins(coins, amount - coinAmt)
-                if subProbMinCoins > -1:
-                    numCoins.append(subProbMinCoins + 1)
+        dp[0] = 0 # zero coins to add up to zero
         
-        if len(numCoins) == 0:
+        # populate the dp array
+        for i in range(1, amount + 1):
+            # iterate through the different coin denominations and take the minimum
+            for coin in coins:
+                # it's only possible to use this coin if it's less than or equal to i,
+                # since i is the current amount
+                if coin <= i:
+                    # update dp[i] if we find a way to use fewer coins
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+                    
+        if dp[amount] == 10001:
             return -1
         
-        return min(numCoins)
-    
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        return Solution.minNumCoins(coins, amount)
+        return dp[amount]
