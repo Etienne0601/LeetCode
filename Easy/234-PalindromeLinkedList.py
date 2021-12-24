21228dc02c47fd1510c96dada249aa3dcac55296
Etienne Robin <etienner0601@gmail.com>
2021-12-23 21:45:23 -0800

    added comments from leetcode at the top to 234

--- Easy/234-PalindromeLinkedList.py
+++ Easy/234-PalindromeLinkedList.py
@@ -1,3 +1,8 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         # first use the tortoise and the hare algorithm to get
