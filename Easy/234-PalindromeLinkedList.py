1d45c954ba361e5a9d77bb3d5e3e3fe43126f9a6
Etienne Robin <etienner0601@gmail.com>
2021-12-23 21:42:36 -0800

    started 234 palindrome linked list in python

---
+++ Easy/234-PalindromeLinkedList.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # first use the tortoise and the hare algorithm to get
+        # a pointer to the midpoint of the linked list
+        slow = head
+        fast = head
+        
+        while fast != None and fast.next != None:
+            # move slow along once and fast along twice
+            slow = slow.next
+            fast = fast.next.next
+        
+        # save the midpoint
+        midpoint = slow
+        
+        # now reverse the second half of the linked list
+        prev = None
+        while slow != None:
+            
