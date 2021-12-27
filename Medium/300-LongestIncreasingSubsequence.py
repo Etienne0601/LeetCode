19d40a5689ae48c1bbaa90a989a2c4a95fb73c97
Etienne Robin <etienner0601@gmail.com>
2021-12-26 18:27:06 -0800

    solved 300 using DP but differently than the earlier recursion

--- Medium/300-LongestIncreasingSubsequence.py
+++ Medium/300-LongestIncreasingSubsequence.py
@@ -1,18 +1,18 @@
 class Solution:
     
-    def lenOfLISWithMax(self, nums: List[int], maxVal: int) -> int:
-        # if there's only one element, return one if it's strictly less than maxVal
-        # else, return 0
-        if len(nums) == 1:
-            return int(nums[0] < maxVal)
-        lastInd = len(nums) - 1
-        # we have two cases - if we include the last element or if we don't
-        # we can only include the last element if it's strictly less than maxVal
-        includedMaxLen = -1
-        if nums[lastInd] < maxVal:
-            includedMaxLen = 1 + self.lenOfLISWithMax(nums[:lastInd], nums[lastInd])
-        
-        return max(includedMaxLen, self.lenOfLISWithMax(nums[:lastInd], maxVal))
-    
     def lengthOfLIS(self, nums: List[int]) -> int:
-        return self.lenOfLISWithMax(nums, 10001)
+        # dp[i] represent the LIS starting at index i in nums
+        # dp[i] is always going to be at minimum 1, which is just the one element
+        dp = [1] * len(nums)
+        # fill dp in backwards order
+        for i in range(len(nums) - 1, -1, -1):
+            # potentially all the indices that come after i could be the next element
+            for j in range(i + 1, len(nums)):
+                # the element at j could come after the element at i
+                # in the LIS only if nums[i] < nums[j]
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+        
+        # then we just want to return the max value in dp, since
+        # the LIS can start at any index in nums
+        return max(dp)
