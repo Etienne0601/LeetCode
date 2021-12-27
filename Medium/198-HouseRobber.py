8eee8065d177b4b4512cd46ba9105e9680975898
Etienne Robin <etienner0601@gmail.com>
2021-12-27 01:39:05 -0800

    solved 198 house robber using dynamic programming

--- Medium/198-HouseRobber.py
+++ Medium/198-HouseRobber.py
@@ -1,14 +1,16 @@
 class Solution:
     
-    def robRecursion(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
+    def rob(self, nums: List[int]) -> int:
+        
         if len(nums) == 1:
             return nums[0]
         
-        included = nums[0] + self.robRecursion(nums[2:])
-        notInclu = self.robRecursion(nums[1:])
-        return max(included, notInclu)
-    
-    def rob(self, nums: List[int]) -> int:
-        return self.robRecursion(nums)
+        # dp[i] represents the max amount you can rob if you only
+        # have the first i+1 houses available to you
+        dp = [nums[0], max(nums[0], nums[1])]
+        
+        for i in range(2, len(nums)):
+            # we have two cases - if we include house i or not
+            dp.append(max(dp[i - 1], nums[i] + dp[i - 2]))
+        
+        return dp[-1]
