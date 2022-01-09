d382cce68f65d247e1edaa76aebf95ce712d5239
Etienne Robin <etienner0601@gmail.com>
2022-01-09 01:10:13 -0800

    made an optimization to 42 trapping rain water

--- Hard/42-TrappingRainWater.py
+++ Hard/42-TrappingRainWater.py
@@ -1,26 +1,30 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # len(height) is 1, then we can't trap water
+        # if len(height) is 1, then we can't trap water
         if len(height) == 1:
             return 0
-        
+
         water = 0
         leftInd = 0
         rightInd = len(height) - 1
         leftPeak = height[0]
         rightPeak = height[-1]
-        
+
         while leftInd < rightInd:
             # shift the smaller peak index towards the center
             if leftPeak <= rightPeak:
-                # add the water that can be held at the (leftInd + 1) index
                 leftInd += 1
-                water += max(0, leftPeak - height[leftInd])
-                leftPeak = max(leftPeak, height[leftInd])
+                # if leftPeak > height[leftInd] then add the new water
+                if leftPeak > height[leftInd]:
+                    water += leftPeak - height[leftInd]
+                else:
+                    leftPeak = height[leftInd]
             else:
-                # add the water that can be held at the (rightInd - 1) index
                 rightInd -= 1
-                water += max(0, rightPeak - height[rightInd])
-                rightPeak = max(rightPeak, height[rightInd])
-                
+                # if rightPeak > height[rightInd] then add the new water
+                if rightPeak > height[rightInd]:
+                    water += rightPeak - height[rightInd]
+                else:
+                    rightPeak = height[rightInd]
+
         return water
