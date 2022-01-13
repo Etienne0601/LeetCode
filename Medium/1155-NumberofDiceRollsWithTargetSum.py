b0ced36487100ce31525472cd252595dc2ad7420
Etienne Robin <etienner0601@gmail.com>
2022-01-13 15:05:44 -0800

    made memory optimization for 1155 solution

--- Medium/1155-NumberofDiceRollsWithTargetSum.py
+++ Medium/1155-NumberofDiceRollsWithTargetSum.py
@@ -1,19 +1,20 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         # dp array
-        dp = [[0 for i in range(target)] for j in range(n)]
+        dp = [0 for i in range(target)]
         
         # initialize with the first die
         for i in range(min(k, target)):
-            dp[0][i] = 1
+            dp[i] = 1
         
         # now do the rest of the dice
-        for i in range(2, n + 1):
-            for j in range(2, target + 1):
-                # dp[i][j] = dp[i-1][j-1] + d[i-1][j-2] + ... + dp[i-1][j-k]
+        for i in range(n - 1):
+            for j in range(target, 0, -1):
+                # count the number of possibilites for this number of
+                # dice and target j
+                dp[j - 1] = 0
                 for val in range(1, k + 1):
                     if j > val:
-                        dp[i - 1][j - 1] += dp[i - 2][j - val - 1]
-                        dp[i - 1][j - 1] %= 1000000007
+                        dp[j - 1] += dp[j - val - 1]
                         
-        return dp[n - 1][target - 1]
+        return dp[-1] % 1000000007
