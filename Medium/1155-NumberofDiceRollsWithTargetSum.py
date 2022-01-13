6a595aa7d9d378d67d1e9feae809b0966f397c1f
Etienne Robin <etienner0601@gmail.com>
2022-01-13 13:57:34 -0800

    solved 1155 using 2D dp array

--- Medium/1155-NumberofDiceRollsWithTargetSum.py
+++ Medium/1155-NumberofDiceRollsWithTargetSum.py
@@ -1,15 +1,19 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        # base cases
-        if n == 0:
-            if target == 0:
-                return 1
-            else:
-                return 0
+        # dp array
+        dp = [[0 for i in range(target)] for j in range(n)]
         
-        # recursive case
-        numWays = 0
-        for i in range(1, k + 1):
-            numWays += self.numRollsToTarget(n - 1, k, target - i)
+        # initialize with the first die
+        for i in range(min(k, target)):
+            dp[0][i] = 1
         
-        return numWays
+        # now do the rest of the dice
+        for i in range(2, n + 1):
+            for j in range(2, target + 1):
+                # dp[i][j] = dp[i-1][j-1] + d[i-1][j-2] + ... + dp[i-1][j-k]
+                for val in range(1, k + 1):
+                    if j > val:
+                        dp[i - 1][j - 1] += dp[i - 2][j - val - 1]
+                        dp[i - 1][j - 1] %= 1000000007
+                        
+        return dp[n - 1][target - 1]
