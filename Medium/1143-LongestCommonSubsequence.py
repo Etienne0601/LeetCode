613b39e19a9cb054527849b7eb44a9aed10ad73e
Etienne Robin <etienner0601@gmail.com>
2022-01-12 19:36:23 -0800

    solved 1143 using 2D dp array

--- Medium/1143-LongestCommonSubsequence.py
+++ Medium/1143-LongestCommonSubsequence.py
@@ -1,12 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        if len(text1) == 0 or len(text2) == 0:
+        length1 = len(text1)
+        length2 = len(text2)
+        if length1 == 0 or length2 == 0:
             return 0
         
-        if text1[0] == text2[0]:
-            return 1 + self.longestCommonSubsequence(text1[1:], text2[1:])
-
-        branch1 = self.longestCommonSubsequence(text1[1:], text2)
-        branch2 = self.longestCommonSubsequence(text1, text2[1:])
+        dp = [[0 for i in range(length2 + 1)] for j in range(length1 + 1)]
         
-        return max(branch1, branch2)
+        for i in range(1, length1 + 1):
+            for j in range(1, length2 + 1):
+                # what is the longest common subsequence of the
+                # first i characters in text1 and the first j
+                # characters in text2
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[length1][length2]
+
