55f5afa551fd735b89839922baa91fee0cb442a8
Etienne Robin <etienner0601@gmail.com>
2022-06-19 18:58:31 -0700

    found a linear time, constant space solution to 26

--- Easy/26-RemoveDuplicatesFromSortedArray.py
+++ Easy/26-RemoveDuplicatesFromSortedArray.py
@@ -1,8 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        for i in range(len(nums) - 1):
-            j = i + 1
-            while j < len(nums) and nums[j] == nums[i]:
-                nums.pop(j)
-        return len(nums)
+        curr = nums[0]
+        currInd = 0
+        for i in range(1, len(nums)):
+            if nums[i] != curr:
+                curr = nums[i]
+                currInd += 1
+                nums[currInd] = curr
+        return currInd + 1
 
